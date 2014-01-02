3b2038a7b1c7a959fd8549e2f31e300ad331ccb7
Marcel Greter <marcel.greter@ocbnet.ch>
2014-01-02 01:59:28 +0100

    Implements hook for comment readers
    We will use this to implement options parsing.

--- lib/OCBNET/CSS3/Comments.pm
+++ lib/OCBNET/CSS3/Comments.pm
@@ -12,9 +12,51 @@ use warnings;
 use base 'OCBNET::CSS3';
 ####################################################################################################
 
+our @readers;
+
+####################################################################################################
+
+# static function only
+# never call as object
+sub register
+{
+
+	# get input arguments
+	my ($reader) = @_;
+
+	# add additional reader
+	push @readers, $reader;
+
+}
+# EO fn register
+
+####################################################################################################
+
 # static getter
 #**************************************************************************************************
 sub type { return 'comment' }
+
+####################################################################################################
+
+# set the readed comment
+# parse keys and values
+sub set
+{
+
+	# get input arguments
+	my ($self, $text) = @_;
+
+	# call super class method
+	$self->SUPER::set($text);
+
+	# call each reader for the given text
+	$_->($self, $text) foreach @readers;
+
+	# instance
+	return $self;
+
+}
+# EO sub set
 
 ####################################################################################################
 
